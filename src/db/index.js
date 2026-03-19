2a6e75a7134f98a453f45c686cf11c4ec2d92f2e
Shreeram Haridas <shreeramkh100@gmail.com>
2026-03-19 19:58:12 +0530

    Enhanced the DB and Server logic with Main control in index.js

--- src/db/index.js
+++ src/db/index.js
@@ -2,13 +2,9 @@ import mongoose from "mongoose"
 import {DB_NAME} from "../constants.js"
 
 export const handleDBConnection=async()=>{
-    try{
-        const dbInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log("DB Connection Successful",dbInstance)
-    }catch(err){
-        console.error("DB Connection Failed",err)
-        process.exit(1) //Shut down(Critical failure)
-    }
+    const dbInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    return dbInstance;     
 }
+//Main Control is Provided in index.js
 //1) Always implement Try Catch
 //2) Always use Async Await //DB is in another continent
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import express from 'express';
 import dotenv from 'dotenv'
-import {handleDBConnection} from "./db/index.js"
-
 //Used to get env values ASAP
-dotenv.config()
+dotenv.config()//✅ ALWAYS first
 
-//DB Connection
-handleDBConnection()
+import express from 'express';
+import {handleDBConnection} from "./db/index.js"
+import {app} from './app.js'
+
+const PORT = process.env.PORT||4501
+
+const startServer=async()=>{
+    try {
+        // 1. Connect DB
+        const db=await handleDBConnection();
+        console.log(`MongoDB Connected: ${db.connection.host}`)
+
+        // 2. Start server
+        const server=app.listen(PORT,()=>{
+            console.log(`Server is running on ${PORT}`)
+        })
+
+        // 3. Handle server-level errors
+        server.on('error',(err)=>{
+            console.error(`Server Error: ${err}`)
+            process.exit(1);//Shut down(Critical failure)
+        })
+
+    } catch (error) {
+        // 4. Handle startup failure (DB or anything in try)
+        console.error(`Startup Failed: ${error}`) 
+        process.exit(1); //Shut down(Critical failure)
+    }
+}
+
+startServer()
